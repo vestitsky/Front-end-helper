e681984af10c5aee05f75b037896133b8633c205
vestitsky <40118064+vestitsky@users.noreply.github.com>
2021-06-08 20:32:35 +0300

    UI - DAL refactoring

--- ajax/index.js
+++ ajax/index.js
@@ -2,19 +2,14 @@ const resultBlock = document.querySelector("#result");
 const pageNumberEl = document.querySelector("#page-number");
 const clickMeButton = document
 	.querySelector("#click-me")
-	.addEventListener("click", makeRequest);
+	.addEventListener("click", () => {
+		getImages(pageNumberEl.value, onDataRecieved);
+	});
 
-function makeRequest() {
-	$.ajax(
-		`https://repetitora.net/api/JS/Images?page=${pageNumberEl.value}&count=1`,
-		{
-			success: function (data) {
-				data.forEach((element) => {
-					const img = document.createElement("img");
-					img.src = element.thumbnail;
-					document.querySelector("#result").appendChild(img);
-				});
-			},
-		}
-	);
+function onDataRecieved(data) {
+	data.forEach((element) => {
+		const img = document.createElement("img");
+		img.src = element.thumbnail;
+		document.querySelector("#result").appendChild(img);
+	});
 }
---
+++ ajax/service.js
@@ -0,0 +1,5 @@
+function getImages(pageNumber, successCallback) {
+	$.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`, {
+		success: successCallback,
+	});
+}
